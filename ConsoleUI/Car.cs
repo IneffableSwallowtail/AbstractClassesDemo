da64d0cafd6ee606ced1c222f42f48966bd9a33f
A.J. Miller <ajmiller1206@gmail.com>
2022-09-26 19:46:23 -0400

    added car and motorcycle classes

---
+++ ConsoleUI/Car.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    internal class Car : Vehicle
+    {
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("You are driving in your car.");
+        }
+
+        public int Doors { get; set; }
+    }
+}
---
+++ ConsoleUI/Motorcycle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public class Motorcycle : Vehicle
+    {
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("You are riding on your motorcycle.");
+        }
+        public override void DriveVirtual()
+        {
+            Console.WriteLine("The wind whips against your helmet.");
+        }
+
+        public string Type { get; set; }
+    }
+}

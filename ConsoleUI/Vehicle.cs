1c98b8a37ca59f83fc9febeb2f04f199e9e24053
A.J. Miller <ajmiller1206@gmail.com>
2022-09-26 19:36:56 -0400

    first commit, vehicle class created

---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public abstract class Vehicle
+    {
+        public int year = 2010;
+        public string make = "Toyota";
+        public string model = "Camry";
+
+        public abstract void DriveAbstract();
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine($"You are driving in your vehicle.");
+        }
+    }
+}
